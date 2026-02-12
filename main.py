332603f25beffc158ee4842e938cfbee3c019e6b
glabpep <contatoglabpeptides@gmail.com>
2026-02-11 21:19:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import hmac
 import hashlib
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": "*", "methods": ["GET", "POST", "OPTIONS"], "allow_headers": ["Content-Type", "Authorization"]}})
 
 # O Render vai ler essas chaves das 'Environment Variables' que configuramos
 INFINITE_TOKEN = os.environ.get("INFINITE_TOKEN", "glabpeplog")
@@ -90,5 +90,6 @@ if __name__ == '__main__':
     # No Render a porta é definida automaticamente pela variável PORT
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
 
 
